86b9105eddcccf4c2e2331e208e3e9df47283b63
janae-b <janae.boersma@comcast.net>
2021-04-27 15:40:01 -0500

    complete lab

--- python-functions.py
+++ python-functions.py
@@ -20,17 +20,25 @@ print('Answer for Problem-2:',largest([10, 4, 2, 231, 91, 54]))
 
 # Problem 3 - Write a function named `occurances` that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
 
-# def occurances ()
 
-# print('Answer for Problem-3:',occurances('fleep floop', 'e'))
-# print('Answer for Problem-3:',occurances('fleep floop', 'p'))
-# print('Answer for Problem-3:',occurances('fleep floop', 'ee'))
-# print('Answer for Problem-3:',occurances('fleep floop', 'fe'))
+def occurances (words, letter):
+  count = words.count(letter)
+  return count
+
+print('Answer for Problem-3:',occurances('fleep floop', 'e'))
+print('Answer for Problem-3:',occurances('fleep floop', 'p'))
+print('Answer for Problem-3:',occurances('fleep floop', 'ee'))
+print('Answer for Problem-3:',occurances('fleep floop', 'fe'))
 
 # Problem 4 - Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product. HINT: Review your notes on args.
 
-# def product (*args)
+def product (*args):
+  number = 1
+  for arg in args:
+    number *= arg
+  return number    
 
-# print('Answer for Problem-4:', product(-1, 4) # returns -4
-# print('Answer for Problem-4:', product(2, 5, 5) # returns -4
-# print('Answer for Problem-4:', product(4, 0.5, 5) # returns -4
+print('Answer for Problem-4:', product(-1, 4)) 
+print('Answer for Problem-4:', product(2, 5, 5)) 
+print('Answer for Problem-4:', product(4, 0.5, 5)) 
+
